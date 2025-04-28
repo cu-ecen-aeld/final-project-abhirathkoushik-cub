bfec28e9a9b7b8c6f0823f20f4108802db07402f
Parth Varsani <pava1107@aesd-students.colorado.edu>
2025-04-28 02:37:02 +0000

    can_tx works fine but can_rx causes problem

--- client_node.sh
+++ client_node.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 FIFO_PATH="/tmp/proxpipe"
+PIPE_CAN_DUMP="/tmp/candump_pipe"
 THRESHOLD=100
 SERVER_CAN_ID="100"
 CLIENT_CAN_ID="101"
@@ -11,18 +12,11 @@ echo "$LED" > /sys/class/gpio/export 2>/dev/null
 echo "out" > /sys/class/gpio/gpio$LED/direction
 echo "0" > /sys/class/gpio/gpio$LED/value
 
-
 # Setup CAN interface
 ip link set can0 up type can bitrate 125000
 
-# Read proximity from FIFO
-read_proximity() {
-    if read -r val < "$FIFO_PATH"; then
-        echo "$val"
-    else
-        echo "0"
-    fi
-}
+# Create FIFO for candump output if needed
+[ -p "$PIPE_CAN_DUMP" ] || mkfifo "$PIPE_CAN_DUMP"
 
 STATE="CAN_RX"
 
@@ -31,32 +25,46 @@ while true; do
         "CAN_RX")
             echo "[Client] State: CAN_RX - Waiting for Exit signal..."
             echo "0" > /sys/class/gpio/gpio$LED/value
-            candump can0 | while read -r iface canid bracket dlc data; do
+
+            candump can0 > "$PIPE_CAN_DUMP" &
+            CANDUMP_PID=$!
+
+            while read -r iface canid bracket dlc data; do
                 if [ "$canid" = "$SERVER_CAN_ID" ]; then
                     val_dec=$((16#$dlc))
                     echo "[Client] Received Exit CAN frame. Value=$val_dec"
                     echo "1" > /sys/class/gpio/gpio$LED/value
                     STATE="CAN_TX"
+                    kill "$CANDUMP_PID"
                     break
                 fi
-            done
+            done < "$PIPE_CAN_DUMP"
             ;;
-
+        
         "CAN_TX")
             echo "[Client] State: CAN_TX - Reading proximity from FIFO and Transmitting back through CAN"
-            prox_val=$(read_proximity)
-            echo "[Client] Proximity: $prox_val"
 
-            if [ "$prox_val" -gt "$THRESHOLD" ]; then
-                frame=$(printf "%02X" "$prox_val")
-                echo "[Client] Sending Entrance signal CAN frame: $frame"
-                cansend can0 $CLIENT_CAN_ID#$frame
-                STATE="CAN_RX"
+            # Make sure /usr/bin/apds_read is running
+            if ! pgrep -f "apds_read" >/dev/null; then
+                echo "[Client] starting /usr/bin/apds_read..."
+                /usr/bin/apds_read &
+                sleep 1
             fi
 
-            sleep 1
-            ;;
+            while read -r val; do
+                val=$(echo "$val" | tr -d '[:space:]')
+                val_hex=$(printf "%02X" "$val")
+                frame="$val_hex"
+                echo "$val, Send=$frame"
 
+                if [ "$val" -gt "$THRESHOLD" ]; then
+                    cansend can0 $CLIENT_CAN_ID#$frame
+                    STATE="CAN_RX"
+                    break
+                fi
+            done < "$FIFO_PATH"
+            ;;
+        
         *)
             echo "Unknown state: $STATE"
             exit 1
--- server_node.sh
+++ server_node.sh
@@ -14,37 +14,34 @@ echo "0" > /sys/class/gpio/gpio$LED/value
 # Setup CAN interface
 ip link set can0 up type can bitrate 125000
 
-# Read proximity from FIFO
-read_proximity() {
-    if read -r val < "$FIFO_PATH"; then
-        echo "$val"
-    else
-        echo "0"
-    fi
-}
+# Start APDS sensor reading
+/usr/bin/apds_read &
 
 STATE="CAN_TX"
 
 while true; do
     case "$STATE" in
         "CAN_TX")
-            echo "[Server] State: CAN_TX - Reading proximity from FIFO and Tranmsmitting over CAN"
+            echo "[Server] State: CAN_TX - Reading proximity from FIFO and Transmitting over CAN"
             echo "0" > /sys/class/gpio/gpio$LED/value
-            prox_val=$(read_proximity)
-            echo "[Server] Proximity: $prox_val"
 
-            if [ "$prox_val" -gt "$THRESHOLD" ]; then
-                frame=$(printf "%02X" "$prox_val")
-                echo "[Server] Sending EXIT signal CAN frame: $frame"
-                cansend can0 $SERVER_CAN_ID#$frame
-                STATE="CAN_RX"
-            fi
+            while read -r val; do
+                val=$(echo "$val" | tr -d '[:space:]')
+                val_hex=$(printf "%02X" "$val")
+                frame="$val_hex"
+                echo "$val, Send=$frame"
 
-            sleep 1
+                if [ "$val" -gt "$THRESHOLD" ]; then
+                    cansend can0 $SERVER_CAN_ID#$frame
+                    STATE="CAN_RX"
+                    break
+                fi
+            done < "$FIFO_PATH"
             ;;
-
+        
         "CAN_RX")
             echo "[Server] State: CAN_RX - Waiting for Entrance signal..."
+
             candump can0 | while read -r iface canid bracket dlc data; do
                 if [ "$canid" = "$CLIENT_CAN_ID" ]; then
                     val_dec=$((16#$dlc))
@@ -55,7 +52,7 @@ while true; do
                 fi
             done
             ;;
-
+        
         *)
             echo "Unknown state: $STATE"
             exit 1
